5a83f755fc32666fc0ee7dd2bfba686f1d9e4f5e
lanrewest <lanrewese@yahoo.com>
2023-06-29 22:15:42 +0000

    gen

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,3 +16,10 @@ def pascal_triangle(n):
     
     return triangle
 
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+print_triangle(pascal_triangle(5))
+
