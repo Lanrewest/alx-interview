82559225ebf0eb95bc088de67ff14f8f90859820
lanrewest <lanrewese@yahoo.com>
2023-07-09 16:51:29 +0000

    lockbos

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):
+def can_unlock_all(boxes):
     """
     Determine if all the boxes can be opened.
 
@@ -38,11 +38,10 @@ def canUnlockAll(boxes):
 
 # Example test cases
 boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # False
+print(can_unlock_all(boxes))  # False
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # True
+print(can_unlock_all(boxes))  # True
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))  # True
-
+print(can_unlock_all(boxes))  # True
